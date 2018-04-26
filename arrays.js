9e75c9332163577d4e3693c3c2e62928b0450389
Sean Logan Para <spara77@gmail.com>
2018-04-26 19:27:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToEndOfArray(array, element) {return array.push(
 
 function accessElementInArray(array, index) {return array[index]}
 
-function destructivelyRemoveElementFromBeginningOfArray(array) { return array.shift()} 
+function destructivelyRemoveElementFromBeginningOfArray(array) { return array.shift(element)} 
 
 function removeElementFromBeginningOfArray(array) {return array.slice(1)}
 
