75dce276e3296c4180a361ab24ac61314a9ad09e
Sean Logan Para <spara77@gmail.com>
2018-04-26 19:36:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {  array.shift();
 
 function removeElementFromBeginningOfArray(array) {return array.slice(1)}
 
-function destructivelyRemoveElementFromEndOfArray(array) {return array.pop()}
+function destructivelyRemoveElementFromEndOfArray(array) { array.pop() ; return array}
 
 function removeElementFromEndOfArray(array) {return array.slice(0, array.length - 1)}
 
